e5c4fbafbe226c99663461c3e7803553f2e9a8ff
Vim <36118616+ps-vivek@users.noreply.github.com>
2022-02-19 12:47:00 -0700

    Added logic to send birthday message and anniversary message

--- src/main/java/com/auto/eventwisher/scheduler/EventWisherScheduler.java
+++ src/main/java/com/auto/eventwisher/scheduler/EventWisherScheduler.java
@@ -2,14 +2,16 @@ package com.auto.eventwisher.scheduler;
 
 import com.auto.eventwisher.Entities.EventConfig;
 import com.auto.eventwisher.Repositories.EventConfigRepository;
+import com.auto.eventwisher.models.WeddingAnniversaryInfoDto;
 import com.auto.eventwisher.services.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
 
 @Component
@@ -22,37 +24,51 @@ public class EventWisherScheduler {
 
     @Scheduled(cron = "0 0 8 * * ?")
     public void scheduledMethod() {
-        //Read from db in paginated way
-        //Send a message
-        String message = "Read Message From Config";
-        String outgoingNumber = "Read number from config";
-
-        //messageService.sendMessage(message,outgoingNumber);
+        fetchEventsAndSendMessages();
     }
 
     //Add error handling and aspect based logging
-    private void fetchEventsAndSendMessages(){
+    private void fetchEventsAndSendMessages() {
         long count = eventConfigRepository.count();
-        int batchSize = (int)count/1000;
+        int batchSize = (int) count / 1000;
 
-        for(int i=0; i<=batchSize; i++) {
-            Pageable paging = PageRequest.of(i, batchSize);
-            Page<EventConfig> eventConfigs = eventConfigRepository.findAll(paging);
+        for (int i = 0; i <= batchSize; i++) {
+            Page<EventConfig> eventConfigs = eventConfigRepository.findAll(PageRequest.of(i, batchSize));
 
-            for (EventConfig eventConfig : eventConfigs.getContent()) {
-                if( Objects.nonNull(eventConfig)){
-                    if( Objects.nonNull(eventConfig.getEventReceiverConfig().getWeddingAnniversaryInfo())){
-                            //Check whether the current date is anniversary date
-                            //If so send message
-                    }
-
-                    if( Objects.nonNull(eventConfig.getEventReceiverConfig().getBirthdayInfo())){
-                        //Check whether the current date is birthdate
-                        //If so send message
-                    }
+            eventConfigs.getContent().forEach(eventConfig -> {
+                if (Objects.nonNull(eventConfig)) {
+                    sendAnniversaryWish(eventConfig);
+                    sendBirthdayWish(eventConfig);
                 }
-
-            }
-
+            });
         }
-}}
+    }
+
+    private void sendBirthdayWish(EventConfig eventConfig) {
+        if (Objects.nonNull(eventConfig.getEventReceiverConfig().getBirthdayInfo())) {
+            if (isToday(eventConfig.getEventReceiverConfig().getBirthdayInfo().getReceiverBirthDate())) {
+                messageService.sendMessage(eventConfig.getEventReceiverConfig().getBirthdayInfo().getCustomBirthdayWish(), eventConfig.getEventReceiverConfig().getReceiverPhoneNumber());
+            }
+        }
+    }
+
+    private void sendAnniversaryWish(EventConfig eventConfig) {
+        if (Objects.nonNull(eventConfig.getEventReceiverConfig().getWeddingAnniversaryInfo())) {
+            if (isToday(eventConfig.getEventReceiverConfig().getWeddingAnniversaryInfo().getReceiverAnniversaryDate())) {
+                WeddingAnniversaryInfoDto weddingAnniversaryInfo = eventConfig.getEventReceiverConfig().getWeddingAnniversaryInfo();
+                weddingAnniversaryInfo.getCustomAnniversaryWish();
+                messageService.sendMessage(weddingAnniversaryInfo.getCustomAnniversaryWish(), eventConfig.getEventReceiverConfig().getReceiverPhoneNumber());
+            }
+        }
+    }
+
+    private boolean isToday(Date date) {
+        Calendar today = Calendar.getInstance();
+
+        Calendar specifiedDate = Calendar.getInstance();
+        specifiedDate.setTime(date);
+
+        return today.get(Calendar.DAY_OF_MONTH) == specifiedDate.get(Calendar.DAY_OF_MONTH)
+                && today.get(Calendar.MONTH) == specifiedDate.get(Calendar.MONTH);
+    }
+}
