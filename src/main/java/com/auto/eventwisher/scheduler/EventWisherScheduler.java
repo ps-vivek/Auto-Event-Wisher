b4625af8ad5785bf662f696b594b8c1f6ecf182a
Vim <36118616+ps-vivek@users.noreply.github.com>
2022-02-28 21:59:35 -0700

    Fixed issues in pagination logic

--- src/main/java/com/auto/eventwisher/scheduler/EventWisherScheduler.java
+++ src/main/java/com/auto/eventwisher/scheduler/EventWisherScheduler.java
@@ -5,6 +5,7 @@ import com.auto.eventwisher.Repositories.EventConfigRepository;
 import com.auto.eventwisher.Entities.WeddingAnniversaryInfoDto;
 import com.auto.eventwisher.services.MessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,24 +16,23 @@ import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EventWisherScheduler {
 
-    private final MessageService messageService;
+    //Make it configurable
+    public static final int BATCH_SIZE = 1000;
 
+    private final MessageService messageService;
     private final EventConfigRepository eventConfigRepository;
 
     @Scheduled(cron = "0 0 8 * * ?")
-    public void scheduledMethod() {
-        fetchEventsAndSendMessages();
-    }
-
     //Add error handling and aspect based logging
     private void fetchEventsAndSendMessages() {
+        log.info("Executing fetchEventsAndSendMessages()");
         long count = eventConfigRepository.count();
-        int batchSize = (int) count / 1000;
-
-        for (int i = 0; i <= batchSize; i++) {
-            Page<EventConfig> eventConfigs = eventConfigRepository.findAll(PageRequest.of(i, batchSize));
+        int totalNumberOfPages = (int) count / BATCH_SIZE;
+        for (int i = 0; i <= totalNumberOfPages; i++) {
+            Page<EventConfig> eventConfigs = eventConfigRepository.findAll(PageRequest.of(i, BATCH_SIZE));
 
             eventConfigs.getContent().forEach(eventConfig -> {
                 if (Objects.nonNull(eventConfig)) {
@@ -65,6 +65,5 @@ public class EventWisherScheduler {
         LocalDate currentDate = LocalDate.now();
         return eventDate.getDayOfMonth() == currentDate.getDayOfMonth() && eventDate.getMonth() == currentDate.getMonth();
     }
-
 
 }
